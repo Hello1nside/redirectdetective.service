65f34c5e33e48dcdec27731662296a96db92b414
Yevhenii Mudrahel <yevhenii.mudrahel@creatoriq.com>
2022-03-19 22:29:23 +0200

    Added processing of nonexistent domains

--- redirectdetective.go
+++ redirectdetective.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type Redirect struct {
-	Url		string `json:"url"`
-	Code	int `json:"code"`
+	Url  string `json:"url"`
+	Code int    `json:"code"`
 }
 
 type Response struct {
-	Status		bool `json:"status"`
-	Response 	[]Redirect `json:"response"`
+	Status   bool       `json:"status"`
+	Response []Redirect `json:"response"`
 }
 
 func (response *Response) AddRedirect(item Redirect) []Redirect {
-    response.Response = append(response.Response, item)
-    return response.Response
+	response.Response = append(response.Response, item)
+	return response.Response
 }
 
 func getRedirects(response *Response, site string) {
@@ -27,47 +27,58 @@ func getRedirects(response *Response, site string) {
 	nextURL := site
 	var i int
 
-	client := &http.Client {
-	  CheckRedirect: func(req *http.Request, via []*http.Request) error {
-	    return http.ErrUseLastResponse
-	} }
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}}
 
 	for i < 100 {
 
 		resp, err := client.Get(nextURL)
-	    if err != nil {
-	      fmt.Println(err)
-	    }
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		redirect := Redirect{resp.Request.URL.String(), resp.StatusCode}
 		response.AddRedirect(redirect)
 
 		if resp.StatusCode == 200 {
 			fmt.Println(resp)
-		 	fmt.Println("Done!")
+			fmt.Println("Done!")
 			break
 		} else if resp.StatusCode == 404 {
 			fmt.Println("404")
-		  	break
+			break
 		} else {
-		  nextURL = resp.Header.Get("Location")
-		  i += 1
+			nextURL = resp.Header.Get("Location")
+			i += 1
 		}
 	}
 }
 
+func responseWriter(w http.ResponseWriter, data string) {
+	resp := make(map[string]string)
+	resp["message"] = data
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)
+	return
+}
+
 func handleRequest(w http.ResponseWriter, req *http.Request) {
 	site := req.URL.Query().Get("site")
-	if len(site) < 3 {
-		resp := make(map[string]string)
-		resp["message"] = "GET parameter `site` is required"
 
-		jsonResp, err := json.Marshal(resp)
-		if err != nil {
-			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonResp)
+	if len(site) < 3 {
+		responseWriter(w, "GET parameter `site` is required")
+		return
+	}
+
+	_, err := http.Get(site)
+	if err != nil {
+		responseWriter(w, "no such host")
 		return
 	}
 
